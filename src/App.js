d36f101d184995c5acce9cb5e559f8baa889d53f
Luv-83 <luvraghuvanshi83@gmail.com>
2026-01-05 19:12:10 +0400

    Clean repo without secrets

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div style={{ padding: 40 }}>
+      <h1>🚀 CI/CD React App</h1>
+      <p>Deployed automatically using GitHub Actions</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
